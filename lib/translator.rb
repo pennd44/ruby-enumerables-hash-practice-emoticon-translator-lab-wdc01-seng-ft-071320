bc843920bc608814f9caf1a265ee88884f56f286
Penn M Diba <pdiba@oxbowschool.org>
2020-06-29 01:38:57 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,39 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+def load_library (file)
   # code goes here
+  emoticons=YAML.load_file(file)
+  new_emoticon_hash={}
+  emoticons.each do |key, value|
+    new_emoticon_hash[key]={}
+    new_emoticon_hash[key][:english]=value[0]
+    new_emoticon_hash[key][:japanese]=value[1]
+  end
+  new_emoticon_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon (file, emoticon)
   # code goes here
+  new_emoticon_hash=load_library(file)
+  japanese_emoticon=""
+  new_emoticon_hash.each do |key|
+    if new_emoticon_hash[key][:english]==emoticon
+      japanese_emoticon=new_emoticon_hash[key][:japanese]
+    end
+  end
+  japanese_emoticon
 end
 
-def get_english_meaning
+def get_english_meaning(file, emoticon)
   # code goes here
+  new_emoticon_hash=load_library(file)
+  english_meaning=""
+  new_emoticon_hash.each do |key|
+    if new_emoticon_hash[key][:japanese]==emoticon
+      english_meaning=key
+    end
+  end
+  english_meaning
 end
